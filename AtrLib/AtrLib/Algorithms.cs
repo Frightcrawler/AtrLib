84412e44c8b56dfb9e13e37aa0d61cd186fe7d93
Tomas <Tomas@TOMAS-PS>
2017-10-07 15:22:26 +0400

    blockingQueue & algorithms initial commit

---
+++ AtrLib/AtrLib/Algorithms.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtrLib
+{
+    public static class Algorithms
+    {
+        public static IDictionary<int, int> SumTwoNumbers(ICollection<int> collection, int x)
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            var temp = new Dictionary<int, bool>();
+            int pairsCount = 0;
+            foreach (var item in collection)
+            {
+                if (item < 0 && x > (int.MaxValue + item))
+                    continue;
+                if (item > 0 && x < (int.MinValue + item))
+                    continue;
+
+                int diff = x - item;
+                if (temp.ContainsKey(diff))
+                {
+                    if (!temp[diff])
+                    {
+                        temp[diff] = true;
+                        pairsCount++;
+                    }
+                }
+                else
+                {
+                    if (!temp.ContainsKey(item))
+                    {
+                        temp.Add(item, false);
+                    }
+                }
+            }
+            var result = new Dictionary<int, int>(pairsCount);
+            foreach (var item in temp)
+            {
+                if (item.Value)
+                {
+                    result.Add(item.Key, x - item.Key);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AtrLib/AtrLib/BlockingQueue.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace AtrLib
+{
+    public class BlockingQueue<T> : IBlockingQueue<T>, IDisposable
+    {
+        private readonly Queue<T> queue;
+        private readonly object syncObj = new object();
+
+        private readonly CancellationToken cancelationToken;
+        private readonly ManualResetEvent queueEvent;
+
+        private readonly WaitHandle[] handles;
+
+        private bool disposed = false;
+
+        #region .ctor & dispose        
+
+        public BlockingQueue(CancellationToken cancelationToken)
+            : this(new Queue<T>(), cancelationToken)
+        {
+        }
+
+        public BlockingQueue(int capacity, CancellationToken cancelationToken)
+            : this(new Queue<T>(capacity), cancelationToken)
+        {
+        }
+
+        public BlockingQueue(IEnumerable<T> collection, CancellationToken cancelationToken)
+            : this(new Queue<T>(collection), cancelationToken)
+        {
+        }
+
+        protected BlockingQueue(Queue<T> queue, CancellationToken cancelationToken)
+        {
+            if (queue == null)
+                throw new ArgumentNullException(nameof(queue));
+
+            this.queue = queue;
+
+            this.queueEvent = new ManualResetEvent(false);
+            this.cancelationToken = cancelationToken;
+            this.handles = new WaitHandle[] { cancelationToken.WaitHandle, queueEvent };
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    lock (syncObj)
+                    {
+                        if (!disposed)
+                        {
+                            disposed = true;
+
+                            queueEvent.Set();
+                            queueEvent.Dispose();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+
+        #region IBlockingQueue<T>
+
+        public T Pop()
+        {
+            T result = default(T);
+            while (true)
+            {
+                lock (syncObj)
+                {
+                    if (disposed)
+                        throw new ObjectDisposedException("BlockingQueue");
+
+                    if (queue.Any())
+                    {
+                        result = queue.Dequeue();
+                        if (!queue.Any())
+                        {
+                            if (!queueEvent.Reset())
+                            {
+                                throw new InvalidOperationException("Cannot reset queueEvent");
+                            }
+                            else
+                            {
+                                Trace.TraceInformation("[BlockingQueue] Reset queueEvent");
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                int wr = WaitHandle.WaitAny(handles);
+                if (wr == 0) //cancel
+                {
+                    Trace.TraceInformation("[BlockingQueue] Cancellation requested");
+                    cancelationToken.ThrowIfCancellationRequested();
+                }
+                else
+                {
+                    Trace.TraceInformation($"[BlockingQueue] Wait complete, res = {wr}");
+                }
+            }
+            return result;
+        }
+
+        public void Push(T item)
+        {
+            lock (syncObj)
+            {
+                if (disposed)
+                    throw new ObjectDisposedException("BlockingQueue");
+
+                bool isEmpty = !queue.Any();
+
+                queue.Enqueue(item);
+
+                if (isEmpty)
+                {
+                    if (!queueEvent.Set())
+                    {
+                        throw new InvalidOperationException("Cannot set queueEvent");
+                    }
+                    else
+                    {
+                        Trace.TraceInformation("[BlockingQueue] Set queueEvent");
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
